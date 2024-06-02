3f8e7e535e56d82ae3596995cb1705f711e65b93
lyhua-tech <slyhour69@gmail.com>
2024-06-02 16:22:22 +0700

    display the animals

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,16 @@ import './index.css';
 
 
 function getRandomAnimal () {
-  const animalArr = ['cow', 'bird', 'horse', 'cat', 'giraffe'];
-  return animalArr[Math.floor(Math.random() * animalArr.length)]
+  const animalArr = ['cow', 'bird', 'horse', 'cat', 'gator', 'dog'];
+  const animalImg = []
+  let path = 'src/svg/'
+  for (let animal of animalArr) {
+    animal = animal.concat('.svg')
+    animalImg.push(path.concat(animal));
+  }
+  return animalImg[Math.floor(Math.random() * animalImg.length)]
 }
 const App = () => {
-  
   return (
     <div>
       <AnimalReact getAnimal={getRandomAnimal}/>
--- src/assets/AnimalReact.jsx
+++ src/assets/AnimalReact.jsx
@@ -1,23 +1,45 @@
-import { useState } from 'react'
-import React from 'react'
+import React, { useState } from 'react';
+import heart from '../svg/heart.svg';
+import '../index.css';
 
-const AnimalReact = (props) => {
-    const { getAnimal } = props
-    const [randomAnimal, setRandomAnimal] = useState([])
-    const handleClick = () => {
-        const newAnimal = getAnimal()
-        setRandomAnimal([...randomAnimal, newAnimal])
-    }
+const AnimalReact = ({ getAnimal }) => {
+  const [randomAnimal, setRandomAnimal] = useState([]);
+  const [scales, setScales] = useState({}); // State to manage the scale of each heart
+
+  const handleAddAnimal = () => {
+    const newAnimal = getAnimal();
+    setRandomAnimal([...randomAnimal, newAnimal]);
+    setScales((prevScales) => ({
+      ...prevScales,
+      [newAnimal]: 0 // Initialize scale for new animal
+    }));
+  };
+
+  const handleScale = (animal) => {
+    setScales((prevScales) => ({
+      ...prevScales,
+      [animal]: prevScales[animal] + 1 // Increment scale for the specific animal
+    }));
+  };
+
   return (
-    <div onClick={handleClick}>
-        <button>Animal</button>
-        <ul>
+    <div>
+      <button onClick={handleAddAnimal} className='btnAnimal'>Animal</button>
+      <div className="cardContainer">
         {randomAnimal.map((animal, index) => (
-          <li key={index}>{animal}</li>
+          <div key={index} onClick={() => handleScale(animal)} className="cardBox">
+            <img src={animal} alt="" className='w-full'/>
+            <img
+              className='heartLogo'
+              src={heart}
+              alt=""
+              style={{ width: `${10 + 10 * (scales[animal] || 0)}px` }} // Scale specific to the animal
+            />
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default AnimalReact
+export default AnimalReact;
